6abed2bed62968e8a5c1b769830c5b1d658b41eb
Anshuman-02905 <anshuman.mandal.01@gmail.com>
2023-04-26 11:39:42 +0530

    GTSRB

---
+++ GTSRB/util.py
@@ -0,0 +1,29 @@
+import json
+import keras
+import numpy as np
+import cv2
+
+
+def convert_image(uploaded_file):
+    print('hello')
+    if uploaded_file is not None:
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+        return opencv_image
+    return None
+
+def get_labels(lable_file):
+    dct = dict()
+    with open('Labels.json') as json_file:
+        dct = json.load(json_file)
+    return dct
+
+def get_model(model_file):
+    model=keras.models.load_model('model_GTSRB')
+    return model
+
+def model_predict(model,img):
+    img = cv2.resize(img, (30, 30), interpolation=cv2.INTER_AREA)
+    img = np.array([img])
+    prediction=str(np.argmax(model.predict(img)))
+    return prediction
